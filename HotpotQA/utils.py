c0dac21510af73ab942993deb57680d40cbb6cad
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-20 11:17:54 +0800

    feat:initial work for hotpotqa

--- HotpotQA/utils.py
+++ HotpotQA/utils.py
@@ -1,10 +1,15 @@
 import torch
 import re
+import json
 
-def get_gold_passages(dataset, split="validation"):
-    dataset = dataset[split]
+def get_gold_passages(data_dict):
+    passages = dict(zip(data_dict["context"]["title"], data_dict["context"]["sentences"]))
 
-    return dataset
+    gold_passages = {}
+    for gold_titles in set(data_dict["supporting_facts"]["title"]):
+        gold_passages[gold_titles] = " ".join(passages[gold_titles])
+    
+    return gold_passages
 
 def response_gen(model, tokenizer, input_ids, generation_config):
   with torch.no_grad():
@@ -24,3 +29,9 @@ def extract_ans_t5(ans, ground_truth):
     clean_ans = re.sub("([\<]).*?([\>])", "", ans).strip()
 
     return clean_ans == ground_truth
+
+def write_json(data, path):
+    f = open(path, mode='a', encoding='utf-8')
+    json.dump(data, f, ensure_ascii=False)
+    f.write('\n')
+    f.close()
